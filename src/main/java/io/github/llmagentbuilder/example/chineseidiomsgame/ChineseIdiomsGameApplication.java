8a637ab8e4b1a09745d78c9113a47f5abb345cd3
Fu Cheng <alexcheng1982@gmail.com>
2024-05-08 17:48:27 +0800

    update model

--- src/main/java/io/github/llmagentbuilder/example/chineseidiomsgame/ChineseIdiomsGameApplication.java
+++ src/main/java/io/github/llmagentbuilder/example/chineseidiomsgame/ChineseIdiomsGameApplication.java
@@ -1,13 +1,13 @@
 package io.github.llmagentbuilder.example.chineseidiomsgame;
 
 import io.github.llmagentbuilder.spring.dev.AgentDevConfiguration;
-import io.github.llmagentbuilder.spring.spring.agentcontroller.AgentController;
+import io.github.llmagentbuilder.spring.spring.agentcontroller.AgentControllerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({AgentDevConfiguration.class, AgentController.class})
+@Import({AgentDevConfiguration.class, AgentControllerConfiguration.class})
 public class ChineseIdiomsGameApplication {
 
   public static void main(String[] args) {
